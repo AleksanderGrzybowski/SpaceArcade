295a89ae8ffa36bfc500ad1b195c86c7742cb34e
Your Name <you@example.com>
2014-04-07 17:23:12 +0000

    Border sprite

--- Line.cpp
+++ Line.cpp
@@ -1,13 +1,11 @@
-/*
- * Line.cpp
- *
- *  Created on: 4 kwi 2014
- *      Author: kelog
- */
-
 #include "Line.h"
 
-Line::Line() : shape(sf::Vector2f(CONF_screenWidth, CONF_lineHeight)) {
-	shape.setPosition(0,  CONF_screenHeight*(1-CONF_shipUpLimit));
-	shape.setFillColor(sf::Color::Yellow);
+Line::Line() /*: shape(sf::Vector2f(CONF_screenWidth, CONF_lineHeight))*/ {
+//	shape.setPosition(0,  CONF_screenHeight*(1-CONF_shipUpLimit));
+//	shape.setFillColor(sf::Color::Yellow);
+
+	texture.loadFromFile("Sprites/Line.png", sf::IntRect(0, 0, CONF_screenWidth, CONF_lineHeight));
+	texture.setRepeated(true);
+	setTexture(texture);
+	setPosition(0,  CONF_screenHeight*(1-CONF_shipUpLimit));
 }
--- Line.h
+++ Line.h
@@ -3,15 +3,17 @@
 
 #include <SFML/Graphics.hpp>
 #include "config.h"
+#include "Drawable.h"
 
-class Line {
+class Line : public Drawable {
 public:
-	sf::RectangleShape shape;
+	//sf::RectangleShape shape;
+	sf::Texture texture;
 
 	Line();
 
-	sf::Vector2f getPosition() { return shape.getPosition(); }
-	void draw(sf::RenderWindow& window) { window.draw(shape); }
+	//sf::Vector2f getPosition() { return shape.getPosition(); }
+	//void draw(sf::RenderWindow& window) { window.draw(shape); }
 };
 
 #endif /* LINE_H_ */
