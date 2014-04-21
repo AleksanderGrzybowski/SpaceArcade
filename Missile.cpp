098215ccad0110fac39cbf0f0fdc6b2a650f6e4f
Your Name <you@example.com>
2014-04-21 20:46:38 +0000

    Ref

--- Missile.cpp
+++ Missile.cpp
@@ -2,22 +2,6 @@
 
 Missile::Missile() {}
 
-//void Missile::moveIterate(sf::Time& t) {
-//	sf::Vector2f actPos = getPosition();
-//
-//	// tylko w górę
-//	actPos.y -= getSpeed() * t.asMilliseconds();
-//	setPosition(actPos.x, actPos.y);
-//}
-
-//void Missile::draw(sf::RenderWindow& window) {
-//	if ((animationSpeedClock).getElapsedTime().asMilliseconds() > CONF_animationSpeed) {
-//		sprite.setTexture(tf.getFlip());
-//		animationSpeedClock.restart();
-//	}
-//	Drawable::draw(window);
-//}
-
 void Missile::initialize(int shipx, int shipy) {
 	tf.add(getSpritesString());
 	setTexture(tf.getFlip());
