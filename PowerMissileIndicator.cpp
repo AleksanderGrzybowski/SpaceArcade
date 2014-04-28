3a5dafae896ee8b6f2378f69fc70b924f231255d
Your Name <you@example.com>
2014-04-28 17:08:58 +0000

    Ref

--- PowerMissileIndicator.cpp
+++ PowerMissileIndicator.cpp
@@ -1,2 +1,16 @@
 #include "PowerMissileIndicator.h"
 
+PowerMissileIndicator::PowerMissileIndicator() : rect(sf::Vector2f(100, 20)) {
+	setPosition(0, 30);
+}
+
+void PowerMissileIndicator::update() {
+	double col;
+	if (PowerMissile::missileLimitClock.getElapsedTime().asMilliseconds() > PowerMissile::timeLimit) col = 255;
+	else {
+		col = ((double)PowerMissile::missileLimitClock.getElapsedTime().asMilliseconds()) / ((double)PowerMissile::timeLimit);
+		col *= 256;
+	}
+
+	rect.setFillColor(sf::Color(col, col, col));
+}
--- PowerMissileIndicator.h
+++ PowerMissileIndicator.h
@@ -7,28 +7,22 @@
 class PowerMissileIndicator {
 public:
 
-	sf::RectangleShape rect;
-	PowerMissileIndicator() : rect(sf::Vector2f(100, 20)) {
+	PowerMissileIndicator();
 
-		rect.setFillColor(sf::Color(128, 128, 128));
+	void setPosition(int xpos, int ypos) {
+		rect.setPosition(xpos, ypos);
 	}
 
 	void draw(sf::RenderWindow& window) {
-		rect.setPosition(100, 100);
 		window.draw(rect);
 	}
 
-	void update() {
-		double col;
-		if (PowerMissile::missileLimitClock.getElapsedTime().asMilliseconds() > PowerMissile::timeLimit) col = 255;
-		else {
-			col = ((double)PowerMissile::missileLimitClock.getElapsedTime().asMilliseconds()) / ((double)PowerMissile::timeLimit);
-			col *= 256;
-		}
-		rect.setFillColor(sf::Color(col, col, col));
-		std::cout << col << std::endl;
-	}
+	void update();
+
 	virtual ~PowerMissileIndicator() {}
+
+private:
+	sf::RectangleShape rect;
 };
 
 #endif /* MISSILEINDICATOR_H_ */
