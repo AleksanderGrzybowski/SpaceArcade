aac2c954336c85b4aec433aea54cbc6ec373e172
Your Name <you@example.com>
2014-05-03 14:42:43 +0000

    Ref

--- Line.h
+++ Line.h
@@ -8,12 +8,6 @@
 class Line : public Drawable {
 public:
 	Line();
-
-	int getSize() const { return 1337; } // nieistotne - a przynajmniej nie powinno być
-	int getAnimationSpeed() const { return 100; } // nieistotne, przynajmniej teraz
-//	virtual const std::vector<std::string> getSpritesString() const {
-//		return {"Sprites/Line.png"};
-//	}
 };
 
 #endif /* LINE_H_ */
